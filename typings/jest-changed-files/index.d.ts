8f0fbcdeba0410edcf290a5dfae9740a46f9a415
Ivan Vasilov <vasilov.ivan@gmail.com>
2019-11-13 12:20:31 +0100

    Typings are not needed because jest-changed-files has them.

--- typings/jest-changed-files/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'jest-changed-files' {
-  export function getChangedFilesForRoots(
-    roots: string[],
-    options?: object
-  ): Promise<{ changedFiles: Set<string>; repos: { git: Set<string>; hg: Set<string> } }>
-  export function findRepos(roots: any[]): Promise<{ git: Set<string>; hg: Set<string> }>
-}
